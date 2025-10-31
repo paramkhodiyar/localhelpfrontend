747cf57b28ace62c9af94b0a6b6f8cc84d18f924
Param Khodiyar <paramkhodiyar@Params-MacBook-Pro.local>
2025-10-31 19:41:51 +0530

    Added hosted backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://localhelpbackend-fix.onrender.com/api/auth',
         changeOrigin: true,
         secure: false,
       },
